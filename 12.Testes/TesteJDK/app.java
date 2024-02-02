45bfad3b27f974946781208d86f3867e8b76093f
OFideliss <otavio-fidelis@outlook.com>
2024-02-02 20:48:52 -0300

    02/02/24

---
+++ 12.Testes/TesteJDK/app.java
@@ -0,0 +1,12 @@
+package TesteJDK;
+
+/**
+ * AloMundo
+ */
+public class app { // Abertura da classe AloMundo
+
+    public static void main(String[] args) {
+        System.out.println("Alô, Mundo!!");
+    }
+
+}
